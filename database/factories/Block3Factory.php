cc8a1c2e967bf9851b6330057eee2614752a12fe
asel.dyusenbekova <asel.dyusenbekova@mail.ru>
2021-04-05 21:25:45 +0600

    block3 creating index.blade.php

---
+++ database/factories/Block3Factory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Block3;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class Block3Factory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Block3::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
